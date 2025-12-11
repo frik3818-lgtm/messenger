a976065fdda3ec389a0a98d04850947bb90c6196
frik3818-lgtm <frik3818@gmail.com>
2025-12-11 16:39:18 +0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'discord-messenger-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/firebase-config.js',
+    '/manifest.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js',
+    'https://www.gstatic.com/firebasejs/10.5.0/firebase-auth-compat.js',
+    'https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore-compat.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
