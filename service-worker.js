3594af3c0337987debbc46d735ec5f1b33942e0e
frik3818-lgtm <frik3818@gmail.com>
2025-12-11 16:55:02 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,11 @@
-const CACHE_NAME = 'discord-messenger-v1';
+const CACHE_NAME = 'anubis-messenger-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/style.css',
     '/app.js',
     '/firebase-config.js',
-    '/manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://www.gstatic.com/firebasejs/10.5.0/firebase-app-compat.js',
-    'https://www.gstatic.com/firebasejs/10.5.0/firebase-auth-compat.js',
-    'https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore-compat.js'
+    '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -30,19 +26,5 @@ self.addEventListener('fetch', event => {
                 }
                 return fetch(event.request);
             })
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
     );
 });
